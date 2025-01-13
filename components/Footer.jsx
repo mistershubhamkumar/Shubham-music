db14df33184d70f164421c3fce7309a97af8ee81
sreejanxd <185584456+sreejanxd@users.noreply.github.com>
2025-01-13 12:56:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer className="py-10 mt-10 text-center">
+            <p className="text-sm">Made ❤️ by <a className="hover:underline hover:text-primary" href="">Oyy Sreejan</a><span className="text-primary">.</span></p>
+            <ul className="flex gap-1 items-center justify-center mt-1">
+                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-white rounded-full p-1 px-2" href="https://instagram.com/sreejan_edit" target="_blank">Instagram</a></li>
+                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-white rounded-full p-1 px-2" href="https://wa.me/message/GET4BX5JRTBQK1" target="_blank">Whatsapp</a></li>
+            </ul>
+        </footer>
+    )
+}
